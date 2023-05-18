8ad15b1d7da064718f4b3b3614c2e4ca74853fdf
jeniy01 <dallown@naver.com>
2023-05-18 14:25:10 +0900

    json

--- src/kr/go/gp/test/DAOTest.java
+++ src/kr/go/gp/test/DAOTest.java
@@ -1,11 +1,7 @@
 package kr.go.gp.test;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 
-import kr.go.gp.util.MySQL8;
 import kr.go.gp.test.Sample1;
 
 import org.junit.Test;
@@ -31,7 +27,7 @@ public class DAOTest {
 	public void getRecord() {
 		System.out.println("\n레코드 검색");
 		Sample1 sam = new Sample1();
-		sam = dao.getSample("parkjihyeon");
+		sam = dao.getSample("kimkitae");
 		if(sam!=null){
 			System.out.println("아이디 : "+sam.getItem1());
 			System.out.println("포인트 : "+sam.getItem2());
@@ -56,7 +52,7 @@ public class DAOTest {
 	public void updateTest() {
 		System.out.println("\n레코드 변경");
 		Sample1 sam = new Sample1();
-		sam.setItem1("parkjihyeon");
+		sam.setItem1("kimkitae");
 		sam.setItem2(200);
 		int cnt = dao.updateSample(sam);
 		if(cnt>0){
@@ -71,89 +67,5 @@ public class DAOTest {
 		if(cnt>0){
 			System.out.println("레코드 삭제 성공");
 		}
-	}
-}
-class SampleDAO {
-	public Connection con = null;
-	public PreparedStatement pstmt = null;
-	public ResultSet rs = null;
-	String sql = "";
-	ArrayList<Sample1> samList = new ArrayList<Sample1>();
-	//샘플 목록
-	public ArrayList<Sample1> getSampleList(){
-		try {
-			con = MySQL8.getConnection();
-			pstmt = con.prepareStatement(MySQL8.SAMPLE_SELECT_ALL);
-			rs = pstmt.executeQuery();
-			while(rs.next()){
-				Sample1 sam = new Sample1();
-				sam.setItem1(rs.getString("item1"));
-				sam.setItem2(rs.getInt("item2"));
-				sam.setItem3(rs.getString("item3"));
-				samList.add(sam);
-			}
-		} catch(Exception e){
-			System.out.println("데이터베이스 연동 실패");
-		}
-		return samList;
-	}
-	//샘플 레코드 하나
-	public Sample1 getSample(String item1){
-		Sample1 sam = new Sample1();
-		try {
-			con = MySQL8.getConnection();
-			pstmt = con.prepareStatement(MySQL8.SAMPLE_SELECT_ONE);
-			pstmt.setString(1, item1);
-			rs = pstmt.executeQuery();
-			if(rs.next()){
-				sam.setItem1(rs.getString("item1"));
-				sam.setItem2(rs.getInt("item2"));
-				sam.setItem3(rs.getString("item3"));
-			}
-		} catch(Exception e){
-			System.out.println("데이터베이스 연동 실패");
-		}
-		return sam;
-	}
-	//샘플 추가
-	public int addSample(Sample1 sam){
-		int cnt = 0;
-		try {
-			con = MySQL8.getConnection();
-			pstmt = con.prepareStatement(MySQL8.SAMPLE_INSERT);
-			pstmt.setString(1, sam.getItem1());
-			pstmt.setInt(2, sam.getItem2());
-			cnt = pstmt.executeUpdate();
-		} catch(Exception e){
-			System.out.println("데이터베이스 연동 실패");
-		}
-		return cnt;
-	}
-	//샘플 변경
-	public int updateSample(Sample1 sam){
-		int cnt = 0;
-		try {
-			con = MySQL8.getConnection();
-			pstmt = con.prepareStatement(MySQL8.SAMPLE_UPDATE);
-			pstmt.setInt(1, sam.getItem2());
-			pstmt.setString(2, sam.getItem1());
-			cnt = pstmt.executeUpdate();
-		} catch(Exception e){
-			System.out.println("데이터베이스 연동 실패");
-		}
-		return cnt;
-	}
-	//샘플 삭제
-	public int deleteSample(String item1){
-		int cnt = 0;
-		try {
-			con = MySQL8.getConnection();
-			pstmt = con.prepareStatement(MySQL8.SAMPLE_DELETE);
-			pstmt.setString(1, item1);
-			cnt = pstmt.executeUpdate();
-		} catch(Exception e){
-			System.out.println("데이터베이스 연동 실패");
-		}
-		return cnt;
 	}
 }
